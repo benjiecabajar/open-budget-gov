f6763c9400198525a6126305b49aa04ec7a04c6f
benjiecabajar <benjiejhonaumentarcabajar@example.com>
2025-11-05 23:37:50 +0800

    Implement budget data caching and UI enhancements, Comparison between NEP&GAA and Automatic IP address getter for the API to run the application on mobile using local ip address

--- lib/service/departments.dart
+++ lib/service/departments.dart
@@ -1,24 +1,48 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:budget_gov/model/list_of_departments.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+
+Future<String> getLocalIpAddress() async {
+  try {
+    final interfaces = await NetworkInterface.list();
+    for (var interface in interfaces) {
+      for (var addr in interface.addresses) {
+        if (addr.type.name == 'IPv4' && !addr.isLoopback) {
+          return addr.address;
+        }
+      }
+    }
+  } catch (e) {
+    debugPrint('Error getting IP: $e');
+  }
+  return 'localhost';
+}
 
 Future<List<ListOfAllDepartmets>> fetchListOfAllDepartments({
   required bool withBudget,
   required String year,
   required String type,
 }) async {
+  String baseUrl;
+
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    final ip = await getLocalIpAddress();
+    baseUrl = 'http://$ip:3000/api/v1/departments/';
+  } else {
+    baseUrl = 'http://localhost:3000/api/v1/departments/';
+  }
+
   final queryParams = {
     'withBudget': withBudget.toString(),
     'year': year,
     'type': type,
   };
 
-  final baseUrl = defaultTargetPlatform == TargetPlatform.android
-      ? 'http://192.168.18.193:3000/api/v1/departments/' //My Phone ni sir
-      : 'http://localhost:3000/api/v1/departments/';
-
   final uri = Uri.parse(baseUrl).replace(queryParameters: queryParams);
+
+  debugPrint('Fetching from: $uri');
 
   final response = await http.get(uri);
 
@@ -30,6 +54,7 @@ Future<List<ListOfAllDepartmets>> fetchListOfAllDepartments({
         )
         .toList();
   } else {
+    debugPrint('Failed to load departments: ${response.statusCode}');
     throw Exception('Failed to load departments');
   }
 }
